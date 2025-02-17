75a33e57a8b093e5e942334cc16a544499b3cc55
chiennc4805 <chiennguyen5981@gmail.com>
2025-02-17 20:36:21 +0700

    dangcap

--- DML_ASM_PRJ301.sql
+++ DML_ASM_PRJ301.sql
@@ -15,12 +15,12 @@ VALUES
 INSERT INTO Employee(FirstName, LastName, BirthDate, Gender, Tel, Address, PositionID, DepartmentID) VALUES
 (N'Công Chiến', N'Nguyễn', '20050804', 'Nam', '0343008127', 'Xuân Đỉnh', 1, 1)
 
+UPDATE ACCOUNT
+SET IsAdmin = 1 
+WHERE Username = 'ChienNCGD0001'
+
 SELECT * FROM ACCOUNT
 
 SELECT * FROM POSITION
 
 SELECT * FROM DEPARTMENT	
-
-UPDATE ACCOUNT
-SET IsAdmin = 1 
-WHERE Username = 'ChienNCGD0001'
